dd7fb59b6b0c39029fa490e7fa577de505c02cc1
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-16 16:10:32 +0330

    feat : code is ended

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,3 @@
 package com.company;
-
 public class Main {
-
-    public static void main(String[] args){
-        int a = 1000;
-        float b = 5.004f;
-        System.out.println(a);
-        System.out.println(b);
-
-    }
-}
+    public static void main(String[] args){int a = 1000;float b = 5.004f;System.out.println(a);System.out.println(b);}}
